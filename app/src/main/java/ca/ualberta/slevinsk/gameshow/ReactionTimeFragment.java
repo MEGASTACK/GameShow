efb83ca29ef94eb7b2eaef0f281c87ff8b1c941b
John Slevinsky <john.slev@gmail.com>
2015-09-19 16:27:27 -0600

    initial

---
+++ app/src/main/java/ca/ualberta/slevinsk/gameshow/ReactionTimeFragment.java
@@ -0,0 +1,98 @@
+package ca.ualberta.slevinsk.gameshow;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ReactionTimeFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link ReactionTimeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ReactionTimeFragment extends Fragment {
+
+    private static final String ARG_PAGE = "ARG_PAGE";
+    private int mPage;
+
+//    private OnFragmentInteractionListener mListener;
+
+
+    public static ReactionTimeFragment newInstance(Integer page) {
+        ReactionTimeFragment fragment = new ReactionTimeFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public ReactionTimeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mPage = getArguments().getInt(ARG_PAGE);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_reaction_time, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+
+//    // TODO: Rename method, update argument and hook method into UI event
+//    public void onButtonPressed(Uri uri) {
+//        if (mListener != null) {
+//            mListener.onFragmentInteraction(uri);
+//        }
+//    }
+//
+//    @Override
+//    public void onAttach(Activity activity) {
+//        super.onAttach(activity);
+//        try {
+//            mListener = (OnFragmentInteractionListener) activity;
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException(activity.toString()
+//                    + " must implement OnFragmentInteractionListener");
+//        }
+//    }
+//
+//    @Override
+//    public void onDetach() {
+//        super.onDetach();
+//        mListener = null;
+//    }
+//
+//    /**
+//     * This interface must be implemented by activities that contain this
+//     * fragment to allow an interaction in this fragment to be communicated
+//     * to the activity and potentially other fragments contained in that
+//     * activity.
+//     * <p/>
+//     * See the Android Training lesson <a href=
+//     * "http://developer.android.com/training/basics/fragments/communicating.html"
+//     * >Communicating with Other Fragments</a> for more information.
+//     */
+//    public interface OnFragmentInteractionListener {
+//        // TODO: Update argument type and name
+//        public void onFragmentInteraction(Uri uri);
+//    }
+
+}
---
+++ app/src/main/java/ca/ualberta/slevinsk/gameshow/ReactionTimeFragmentPagerAdapter.java
@@ -0,0 +1,36 @@
+package ca.ualberta.slevinsk.gameshow;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by john on 15-09-19.
+ */
+public class ReactionTimeFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+    private Context context;
+
+    public ReactionTimeFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return ReactionTimeFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
